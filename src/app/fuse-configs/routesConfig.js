e0fa2c4fb840eef191c9ed338b83cb7de883bee3
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-04-02 01:07:53 -0300

    modifica routes para direcionar release

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -1,14 +1,14 @@
 import { Navigate } from 'react-router-dom';
 import FuseUtils from '@fuse/utils';
-import ExampleConfig from 'app/main/example/ExampleConfig';
-import LoginConfig from 'app/main/login/LoginConfig';
-import ProductsConfig from 'app/main/products/ProductsConfig';
-import RegisterConfig from 'app/main/register/RegisterConfig';
+// import ExampleConfig from 'app/main/example/ExampleConfig';
+// import LoginConfig from 'app/main/login/LoginConfig';
+// import ProductsConfig from 'app/main/products/ProductsConfig';
+// import RegisterConfig from 'app/main/register/RegisterConfig';
 import ReleaseConfig from 'app/main/releasePage/ReleaseConfig';
 import FuseLoading from '@fuse/core/FuseLoading';
 import Error404Page from 'app/main/404/Error404Page';
 
-const routeConfigs = [ReleaseConfig, ExampleConfig, ProductsConfig, LoginConfig, RegisterConfig];
+const routeConfigs = [ReleaseConfig];
 
 const routes = [
   // if you want to make whole app auth protected by default change defaultAuth for example:
