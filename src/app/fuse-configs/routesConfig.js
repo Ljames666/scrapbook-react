c6fb812d65d41cec2d4ff074eccf9d8372c193db
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-04-04 19:55:32 -0300

    registra routes

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -1,14 +1,30 @@
 import { Navigate } from 'react-router-dom';
 import FuseUtils from '@fuse/utils';
-// import ExampleConfig from 'app/main/example/ExampleConfig';
-// import LoginConfig from 'app/main/login/LoginConfig';
-// import ProductsConfig from 'app/main/products/ProductsConfig';
-// import RegisterConfig from 'app/main/register/RegisterConfig';
+import HomeConfig from 'app/main/home/HomeConfig';
+import LoginConfig from 'app/main/login/LoginConfig';
+import RegisterConfig from 'app/main/register/RegisterConfig';
 import ReleaseConfig from 'app/main/releasePage/ReleaseConfig';
+import DiaryConfig from 'app/main/my-diary/DiaryConfig';
+import ContactsConfig from 'app/main/my-contacts/ContactsConfig';
+import EmailConfig from 'app/main/my-email/EmailConfig';
+import PicturesConfig from 'app/main/my-pictures/PicturesConfig';
+import ScheduleConfig from 'app/main/my-schedule/ScheduleConfig';
+import TasksConfig from 'app/main/my-tasks/TasksConfig';
 import FuseLoading from '@fuse/core/FuseLoading';
 import Error404Page from 'app/main/404/Error404Page';
 
-const routeConfigs = [ReleaseConfig];
+const routeConfigs = [
+  ReleaseConfig,
+  HomeConfig,
+  ContactsConfig,
+  DiaryConfig,
+  EmailConfig,
+  PicturesConfig,
+  ScheduleConfig,
+  TasksConfig,
+  LoginConfig,
+  RegisterConfig,
+];
 
 const routes = [
   // if you want to make whole app auth protected by default change defaultAuth for example:
@@ -17,7 +33,7 @@ const routes = [
   ...FuseUtils.generateRoutesFromConfigs(routeConfigs, null),
   {
     path: '/',
-    element: <Navigate to="release" />,
+    element: <Navigate to="home" />,
   },
   {
     path: 'loading',
